94e0fbe0fa64f92e255ca6a8999bf5460ef92853
DELL <datnvph14454@fpt.edu.vn>
2021-11-26 09:55:59 +0700

    dat

--- app/src/main/java/com/poly/testgit/MainActivity.java
+++ app/src/main/java/com/poly/testgit/MainActivity.java
@@ -2,7 +2,9 @@ package com.poly.testgit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
