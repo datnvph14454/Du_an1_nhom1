6b3d49a2136d15e0555d003ffe982f5b75a6aa71
admin <hieubvph14441@fpt.edu.vn>
2021-11-26 21:08:12 +0700

    hieu

--- app/src/main/java/com/poly/testgit/LoginActivity.java
+++ app/src/main/java/com/poly/testgit/LoginActivity.java
@@ -3,13 +3,17 @@ package com.poly.testgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+        tv.setText("hello wolrd");
+
+
     }
 }
